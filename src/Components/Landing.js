c19b64299ec6f7c683755535bc3c49e94cb0e83c
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-15 20:26:06 -0400

    Init commit.

---
+++ src/Components/Landing.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Alert } from 'shards-react'
+class Landing extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Landing;
