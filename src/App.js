cace4f8e5cc73d3972bf3767b74fab48887cd492
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-18 16:17:47 -0400

    Refactoring structure.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './Components/Landing/Landing'
 
+
+const EnterButton = () => {
+
+ return (
+  <div  className="button-div">
+   <button className="enter-button">
+    Enter
+   </button>
+  </div>
+
+ )
+}
 class App extends React.Component {
 
  constructor(props) {
@@ -12,15 +24,30 @@ class App extends React.Component {
     onLanding: true
   }
  }
+
+EnterButton = () => {
+
+  return (
+   <div  className="button-div">
+    <button className="enter-button">
+     Enter
+    </button>
+   </div>
  
- changePhase = () => {
-  
+  )
  }
+
  render () {
   return (
-    <>
-{    this.state.onLanding ?  <Landing /> : <div>A word</div> }
-    </>
+   <>
+      <div className="background">
+      <div className="overlay">
+       <div className="title" >La Maison De Lorenzo</div>
+       <EnterButton />
+       <div className="title2" >La Maison De Lorenzo</div>
+      </div>
+     </div>
+   </>
   );
  }
 }
