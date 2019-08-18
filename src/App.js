2c9a02951225dbe5d80bf6eb29239db320701bc1
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-18 18:08:52 -0400

    Oof, gotta reverse some things.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Landing from './Components/Landing/Landing'
+import { Route, Link, Switch } from 'react-router-dom'
 
 
 const EnterButton = () => {
 
  return (
   <div  className="button-div">
-   <button className="enter-button">
+   <div className="enter-button">
     Enter
-   </button>
+   </div>
   </div>
 
  )
@@ -19,31 +19,27 @@ class App extends React.Component {
 
  constructor(props) {
   super(props)
- 
   this.state = {
-    onLanding: true
-  }
+    onLanding: true,
+   }
+  this.handleState = this.handleState.bind(this)
+
  }
 
-EnterButton = () => {
-
-  return (
-   <div  className="button-div">
-    <button className="enter-button">
-     Enter
-    </button>
-   </div>
- 
-  )
+ handleState = () => {
+  this.setState({
+   onLanding: !this.onLanding
+  })
  }
 
  render () {
+  const { onLanding } = this.state
   return (
    <>
       <div className="background">
       <div className="overlay">
        <div className="title" >La Maison De Lorenzo</div>
-       <EnterButton />
+       <EnterButton onClick={() => this.handleState} />
        <div className="title2" >La Maison De Lorenzo</div>
       </div>
      </div>
