6a4f521b199c34b8dbbe1ea88c1c7937ba6594c2
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-24 01:18:12 -0400

    Hey Mikey, I think he likes it - Tank.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import './Home.css'
-import { Box, Button, Grid, Image, Menu, Tabs, Tab } from 'grommet'
-
-
+import { Grommet, Box, Button, Grid, Image, Menu, Tabs, Tab } from 'grommet'
+import { TreeOption } from 'grommet-icons'
 
 class Home extends Component {
  constructor(props) {
@@ -18,31 +17,38 @@ class Home extends Component {
 
 
   render() {
-
+      const theme = {
+      global: {},
+      tabs: {
+        gap: "90px",
+        justify: "start",
+        flex: {
+        "grow" : true,
+        "shrink": false
+        }
+      },
+      }
     return (
       <div>
-        <Tabs className={"nav-tabs"}>
-          <Tab title="tab 1">
-            <Box pad="medium">One</Box>
-          </Tab>
-          <Tab title="tab 2">
-            <Box pad="medium">Two</Box>
-          </Tab>
-        </Tabs>
-
-
-      <Grid
-      className={"grid-content"}
-      rows={["200px", "200px"]}
-      columns={['xsmall', 'medium']}
-      gap={"medium"}
-      areas={[
-        {name: 'nav', start: [0, 0], end: [0, 1]},
-        ]}
-      >
-      <Box gridArea={"nav"} background={"light-5"}/>
-      </Grid>
-
+        <Grommet theme={theme}>
+          <Tabs className={"nav-tabs"}>
+            <Tab title="About">
+              <Box pad="medium">One</Box>
+            </Tab>
+            <Tab title="Projects">
+              <Box pad="medium">Two</Box>
+            </Tab>
+            <Tab title="Art">
+              <Box pad="medium">Two</Box>
+            </Tab>
+            <Tab title="Writing">
+              <Box pad="medium">Two</Box>
+            </Tab>
+            <Tab title="Music">
+              <Box pad="medium">Two</Box>
+            </Tab>
+          </Tabs>
+        </Grommet>
       </div>
     );
   }
