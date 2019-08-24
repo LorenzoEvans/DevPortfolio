89bd1d4bb7dabf0da54c1aa3ede3a06e452bba45
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-23 23:04:44 -0400

    Quite the UI journey.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './Home.css'
-import { Box, Button, Grid, Image, Menu } from 'grommet'
+import { Box, Button, Grid, Image, Menu, Tabs, Tab } from 'grommet'
 
 
 
@@ -21,31 +21,26 @@ class Home extends Component {
 
     return (
       <div>
-      <Grid
-      rows={['70vw', "70vw"]}
-      columns={['100vh', '100vh']}
-      areas={[
-          {name: 'background', start: [0,0], end: [1, 0]}]}
-      >
-        <Box gridArea={"background"}>
+        <Tabs className={"nav-tabs"}>
+          <Tab title="tab 1">
+            <Box pad="medium">One</Box>
+          </Tab>
+          <Tab title="tab 2">
+            <Box pad="medium">Two</Box>
+          </Tab>
+        </Tabs>
 
-        </Box>
 
       <Grid
       className={"grid-content"}
-      rows={['small', 'small']}
+      rows={["200px", "200px"]}
       columns={['xsmall', 'medium']}
       gap={"medium"}
       areas={[
-        {name: 'header', start: [0, 0], end: [1, 0]},
-        {name: 'nav', start: [0, 1], end: [0, 1]},
-        {name: 'main', start: [1, 1], end: [1, 1]}
+        {name: 'nav', start: [0, 0], end: [0, 1]},
         ]}
       >
-      <Box className={"grid-content"} gridArea={"header"} background={"brand"}/>
       <Box gridArea={"nav"} background={"light-5"}/>
-      <Box gridArea={"main"} background={"light-2"} />
-      </Grid>
       </Grid>
 
       </div>
