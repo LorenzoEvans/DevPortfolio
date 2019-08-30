3dd1caf910040cf163e3cf85b40ed180c2c20124
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-29 23:48:54 -0400

    Saving this, keep eye on *here* in future.

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -23,6 +23,8 @@ function Landing()  {
     return (
      <div className="background">
        <span className={"title"} > La Maison De #0xLE
+       <span className={"title3"} >CodeArtLogicBeauty</span>
+
        </span>
        <NavLink className={"enter-button"} exact to="/home" activeStyle={{textDecoration: 'none'}}>
        Enter
