082cbd752d754e372a9d826eb01043e14c0a1d56
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-30 23:00:11 -0400

    Hmmm...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'App.css';
+import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import Landing from './Components/Landing/Landing'
 import HomePage from './Components/Home/Home'
